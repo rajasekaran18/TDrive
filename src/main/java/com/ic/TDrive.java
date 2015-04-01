9649404292c6415571f337e47db059da103066ab
rajasekaran18 <rajasekaran.prasanna@gmail.com>
2015-04-01 18:00:08 -0500

    MapReduce Initial commit

--- src/main/java/com/ic/TDrive.java
+++ src/main/java/com/ic/TDrive.java
@@ -1,32 +1,163 @@
 package com.ic;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class TDrive 
+public class TDrive extends Configured implements Tool 
 {
-    public static class TDriveMapper extends Mapper<Text, Text, Text, TaxiWritable>
+    public static class TDriveMapper extends Mapper<LongWritable, Text, Text, TaxiWritable>
     {
 	 
-	 protected void map(Text key, Text value, Context context) throws IOException, InterruptedException 
-	 {
+    	private Text mapOutputKey = new Text();
+    	private TaxiWritable mapOutputValue= new TaxiWritable();
+    	
+      @Override
+	  protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
+	  {	  
+	    	
 		StringTokenizer tokenizer = new StringTokenizer(value.toString(), ",") ;
 		
 		String taxiID =  tokenizer.nextToken();
-		String date = tokenizer.nextToken();
+		String date = tokenizer.nextToken().split("\\s+")[0];
 		String lat = tokenizer.nextToken();
 		String lon = tokenizer.nextToken();
 		
-		
-		TaxiWritable taxiWritable = new TaxiWritable(Integer.parseInt(taxiID), date, 
-				new Float(lat), new Float(lon));
-		
-		context.write(new Text(taxiID), taxiWritable);
-	  }
+
+		  mapOutputKey.set(taxiID+":"+date);
+		  mapOutputValue.set(new IntWritable(Integer.parseInt(taxiID)), new Text(date), new DoubleWritable(new Float(lat)), new DoubleWritable(new Float(lon)));
+		 
+		  context.write(mapOutputKey, mapOutputValue);	  
+		 
+	   } 	
     }
     
+    
+    public static class TDriveReducer extends Reducer<Text, TaxiWritable, Text, Text>
+    {
+    	private Text reduceOutputVal = new Text();
+    	
+    	@Override
+    	protected void reduce(Text key, Iterable<TaxiWritable> val, Context context) throws IOException, InterruptedException
+    	{
+    		Iterator<TaxiWritable> iterator = val.iterator();
+    		
+    		double dist = 0.0;
+    		TaxiWritable point1 = (TaxiWritable)iterator.next();
+    		
+    		while(iterator.hasNext())
+    		{
+    			
+    			TaxiWritable point2 = (TaxiWritable)iterator.next();
+    			double twoPointDistance = distance(point1.getLat().get(), point1.getLon().get(), point2.getLat().get(), point2.getLon().get());
+    			if(twoPointDistance > 0)
+    			{
+    			  dist = dist + twoPointDistance;    			 
+    			}
+    			else
+    			{
+    				if(iterator.hasNext())
+    				{
+    				 point2 = (TaxiWritable)iterator.next();
+    				}
+    			} 
+    			
+    			point1 = point2;
+    			
+    		}
+    		reduceOutputVal.set(Double.toString(dist));
+    		context.write(key, reduceOutputVal );			
+    	}
+    	
+    } 
+  
+    private static double distance(double lat1, double lon1, double lat2, double lon2)
+    {
+    	try
+    	{
+    		
+    	  double theta = lon1 - lon2;
+    	  double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+    	  dist = Math.acos(dist);
+    	  dist = rad2deg(dist);
+    	  dist = dist * 60 * 1.1515;
+    	  return (dist);
+    	}
+    	catch(Exception ex)
+    	{
+    		return 0;
+    	}  	 
+    	 
+     }
+    
+    private static double deg2rad(double deg)
+    {
+    	  return (deg * Math.PI / 180.0);
+    }
+
+    private static double rad2deg(double rad)
+    {
+    	  return (rad * 180 / Math.PI);
+    }
+
+    public static boolean deleteFile(String filepath, Configuration conf) throws IOException
+	{
+		
+		Path output2 = new Path(filepath);
+ 		FileSystem fs2 = FileSystem.get(conf);
+		return fs2.delete(output2, true);		
+	}
+ 
+    public int run(String[] args) throws Exception
+    {
+    	//delete ouput file to rerun the job more than once
+    	deleteFile(args[1],  getConf());
+    	
+		Configuration conf =new Configuration();
+		Job job = new Job(conf, "TDrive");
+		job.setJarByClass(TDriveMapper.class);
+		
+		job.setInputFormatClass(TextInputFormat.class); 
+		TextInputFormat.addInputPath(job, new Path(args[0]));
+		
+		job.setMapperClass(TDriveMapper.class);
+		job.setReducerClass(TDriveReducer.class);
+		
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(TaxiWritable.class);
+		
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		boolean res = job.waitForCompletion(true);
+		
+		if(res)
+			return 0;
+		else
+			return -1; 	
+	}
+    
+	public static void main(String[] args) throws Exception 
+	{
+		
+		int res = ToolRunner.run(new TDrive(), args);
+		System.exit(res);		
+	}
     
 }
--- src/main/java/com/ic/TaxiWritable.java
+++ src/main/java/com/ic/TaxiWritable.java
@@ -3,35 +3,58 @@ package com.ic;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 
-import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
 
-public class TaxiWritable implements WritableComparable<TaxiWritable> 
-{
+import org.apache.hadoop.io.*;
+
+public class TaxiWritable implements Writable , WritableComparable<TaxiWritable> 
+{	
+	
 	private IntWritable taxiID;
 	private Text date;
-	private FloatWritable lat;
-	private FloatWritable lon;
+	private DoubleWritable lat;
+	private DoubleWritable lon;
+	
+	public TaxiWritable() {
+		// TODO Auto-generated constructor stub
+		this.taxiID = new IntWritable();
+		this.date = new Text();
+		this.lat = new DoubleWritable();
+		this.lon = new DoubleWritable();
+	}
 	
 	public TaxiWritable(int taxiID, String date, float lat, float lon){
 		this.taxiID = new IntWritable(taxiID);
 		this.date = new Text(date);
-		this.lat = new FloatWritable(lat);
-		this.lon = new FloatWritable(lon);
+		this.lat = new DoubleWritable(lat);
+		this.lon = new DoubleWritable(lon);
 	}
 	
+	public void set(IntWritable taxiID, Text date, DoubleWritable lat, DoubleWritable lon)
+	{
+		this.taxiID = taxiID;
+		this.date = date;
+		this.lat = lat;
+		this.lon = lon;
+	}
 		
-	public void readFields(DataInput in) throws IOException {
+	
+	public void readFields(DataInput in) throws IOException
+	{
 		// TODO Auto-generated method stub
+		
 		taxiID.readFields(in);
 		date.readFields(in);
 		lat.readFields(in);
 		lon.readFields(in);
 	}
+	
+	 public static TaxiWritable read(DataInput in) throws IOException {
+		 TaxiWritable taxiWritable = new TaxiWritable();
+		 taxiWritable.readFields(in);
+	     return taxiWritable;
+	    }
+	 
 	
 	public void write(DataOutput out) throws IOException {
 		// TODO Auto-generated method stub
@@ -39,8 +62,28 @@ public class TaxiWritable implements WritableComparable<TaxiWritable>
 		date.write(out);
 		lat.write(out);
 		lon.write(out);
-	}
+	}	  
 	
+	public IntWritable getTaxiID() {
+		return taxiID;
+	}
+
+
+	public Text getDate() {
+		return date;
+	}
+
+
+	public DoubleWritable getLat() {
+		return lat;
+	}
+
+
+	public DoubleWritable getLon() {
+		return lon;
+	}
+
+  
 	public int compareTo(TaxiWritable other) {
 		// TODO Auto-generated method stub
 		int compareVal = this.taxiID.compareTo(other.taxiID);
@@ -50,5 +93,27 @@ public class TaxiWritable implements WritableComparable<TaxiWritable>
 		return this.date.compareTo(other.date);
 	}
 	
-
+	
+	  @Override
+	  public boolean equals(Object o) {
+	    if (o instanceof TaxiWritable) {
+	    	TaxiWritable tp = (TaxiWritable) o;
+	      return taxiID.equals(tp.taxiID) && date.equals(tp.date)
+	    		  &&lat.equals(tp.lat)&&lon.equals(tp.lon);
+	    }
+	    return false;
+	  }
+	  
+	  @Override
+	  public int hashCode() {
+	    return taxiID.get() + lat.hashCode() * 163 + lat.hashCode();
+	  }
+	  
+  
+	  @Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return taxiID+":"+date+":"+lat+":"+lon;
+	}
+	  
 }
